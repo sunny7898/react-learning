2a82457e3f0c8d5b3a12cc4d0abc05b92bbac0f7
Sunny Goswami <sungoswami07gmail.com>
2023-08-12 03:14:20 +0530

    JS revision done

---
+++ 02-javascript/script.js
@@ -0,0 +1,247 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+// const book = getBook(3);
+
+// function getTotalReviewCount(book) {
+//   const goodreadsReviewCount = book.reviews.goodreads.reviewsCount;
+//   const librarythingReviewCount = book.reviews.librarything?.reviewsCount ?? 0;
+//   librarythingReviewCount;
+
+//   return goodreadsReviewCount + librarythingReviewCount;
+// }
+
+// const totalCount = getTotalReviewCount(book);
+// totalCount;
+
+// // console.log(book.reviews.librarything.reviewsCount);
+
+// // const count = book.reviews.librarything.reviewsCount ?? "no data";
+// // count;
+
+// // const newArray = {
+// //   ...book,
+// //   pages: 1200,
+// //   MoviePublicationDate: "2021-10-15",
+// // };
+
+// // const { title, pages, publicationDate, hasMovieAdaptation } = book;
+
+// // const summary = `${title}, a ${pages}-page long book, published in ${
+// //   publicationDate.split("-")[0]
+// // }. The book has ${hasMovieAdaptation ? "" : "not "}been adapted as a movie`;
+
+// const books = getBooks();
+// books;
+
+// const titles = books.map((book) => book.title);
+// titles;
+
+// const essentialData = books.map((book) => {
+//   return {
+//     title: book.title,
+//     author: book.author,
+//   };
+// });
+
+// essentialData;
+
+// const longBooks = books
+//   .filter((book) => book.pages > 500)
+//   .filter((book) => book.hasMovieAdaptation);
+// longBooks;
+
+// const adventureBooks = books
+//   .filter((book) => book.genres.includes("adventure"))
+//   .map((book) => book.title);
+
+// adventureBooks;
+
+// const totalPages = books.reduce((acc, book) => acc + book.pages, 0);
+// totalPages;
+
+// const arr = [13, 7, 14, 11, 2];
+
+// const sortedArr2 = arr.slice().sort((num1, num2) => num1 - num2);
+// sortedArr2;
+// arr;
+
+// const sortedByPages = books
+//   .slice()
+//   .sort((book1, book2) => book1.pages - book2.pages)
+//   .map((book) => {
+//     return {
+//       title: book.title,
+//       pages: book.pages,
+//     };
+//   });
+
+// sortedByPages;
+
+const books = getBooks();
+books;
+
+const newBook = {
+  id: 6,
+  title: "Marima to the heaven",
+  author: "G. K. Sunny",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+const booksAfterDelete = books.filter((book) => book.id !== 1);
+booksAfterDelete;
+
+const booksAfterUpdate = books.map((book) =>
+  book.id === 1
+    ? {
+        ...book,
+        pages: 1,
+      }
+    : book
+);
+
+booksAfterUpdate;
