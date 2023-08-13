b46b70085642e57a70acd9ce13f8a53f8e4c4b04
Sunny Goswami <sungoswami07gmail.com>
2023-08-14 02:48:08 +0530

    profile-card project completed

---
+++ profile-card/src/index.js
@@ -0,0 +1,94 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA"
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D"
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF"
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33"
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB"
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00"
+  }
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="opp.jpg" alt="Oppenheimer" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>R Oppenheimer</h1>
+      <p>
+        Should contain one Skill component for each web dev skill that you have,
+        customized with props
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skillObj={skill} />
+      ))}
+    </div>
+  );
+}
+
+function Skill(props) {
+  let emoji = "💪";
+  if (props.skillObj.level === "intermediate") emoji = "👍";
+  else if (props.skillObj.level === "beginner") emoji = "👶";
+
+  return (
+    <div className="skill" style={{ backgroundColor: props.skillObj.color }}>
+      {`${props.skillObj.skill} ${emoji}`}
+    </div>
+  );
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
