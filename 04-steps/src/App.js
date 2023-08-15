b4cf1086e80ca917c8779b614bfd2faa78e58dda
Sunny Goswami <sungoswami07gmail.com>
2023-08-15 16:00:04 +0530

    steps-project completed

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,48 +1,66 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
-
 export default function App() {
-  const [step, setStep] = useState(1);
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
 
-  const decreaseStep = () => {
-    if (step > 1) setStep(step - 1);
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep((currStep) => currStep - 1);
   };
 
-  const increaseStep = () => {
-    if (step < 3) setStep(step + 1);
+  const handleNext = () => {
+    if (step < 3) {
+      setStep((currStep) => currStep + 1);
+    }
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step === 1 ? "active" : ""}`}>1</div>
-        <div className={`${step === 2 ? "active" : ""}`}>2</div>
-        <div className={`${step === 3 ? "active" : ""}`}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
+      {isOpen ? (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={() => decreaseStep()}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={() => increaseStep()}
-        >
-          Next
-        </button>
-      </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
