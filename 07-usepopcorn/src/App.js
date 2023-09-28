4e9675ff63f21046b82c1bd02bac79b8d720a81e
Sunny Goswami <sungoswami07gmail.com>
2023-09-28 12:07:27 +0530

    Star Rating component completed

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -121,18 +121,6 @@ function Box({ children }) {
     </div>
   );
 }
-// function Box({ element }) {
-//   const [isOpen, setIsOpen] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
-//         {isOpen ? "–" : "+"}
-//       </button>
-//       {isOpen && element}
-//     </div>
-//   );
-// }
 
 function MovieList({ movies }) {
   return (
