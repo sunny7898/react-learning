369d2b25a9e7223ca89e006b5ee9bacb741a35bd
Sunny Goswami <sungoswami07gmail.com>
2023-12-24 16:57:14 +0530

    using the useref for input bar focus

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -17,6 +17,26 @@ function Logo() {
   );
 }
 function Search({ query, setQuery }) {
+  // Create a ref with initial value
+  const inputElement = useRef(null);
+
+  // Using the created ref
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) return;
+
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -24,6 +44,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement} // connecting the input element with the ref
     />
   );
 }
