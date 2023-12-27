eafa861ad1ca311416db3c551c8cbacd24746a8c
Sunny Goswami <sungoswami07gmail.com>
2023-12-27 13:28:13 +0530

    final clean up

--- 08-react-quiz/src/DateCounter.js
+++ 08-react-quiz/src/DateCounter.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
   switch (action.type) {
@@ -10,49 +11,20 @@ function reducer(state, action) {
       return { ...state, count: action.payload };
     case "setStep":
       return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
     default:
       throw new Error("Unknown action type");
   }
 }
 
 function DateCounter() {
-  // const [count, dispatch] = useReducer(reducer, 0);
-  // const [step, setStep] = useState(1);
-
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
-
-  const dec = function () {
-    dispatch({ type: "dec" });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
-  };
-
-  const inc = function () {
-    dispatch({ type: "inc" });
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
-  };
-
-  const defineCount = function (e) {
-    dispatch({ type: "setCount", payload: Number(e.target.value) });
-    // setCount(Number(e.target.value));
-  };
-
-  const defineStep = function (e) {
-    dispatch({ type: "setStep", payload: Number(e.target.value) });
-    // setStep(Number(e.target.value));
-  };
-
-  const reset = function () {
-    // setCount(0);
-    // setStep(1);
-  };
 
   return (
     <div className="counter">
@@ -62,21 +34,32 @@ function DateCounter() {
           min="0"
           max="10"
           value={step}
-          onChange={defineStep}
+          onChange={(e) =>
+            dispatch({ type: "setStep", payload: Number(e.target.value) })
+          }
         />
         <span>{step}</span>
       </div>
 
       <div>
-        <button onClick={dec}>-</button>
-        <input value={count} onChange={defineCount} />
-        <button onClick={inc}>+</button>
+        <button onClick={() => dispatch({ type: "dec" })}>-</button>
+        <input
+          value={count}
+          onChange={(e) =>
+            dispatch({ type: "setCount", payload: Number(e.target.value) })
+          }
+        />
+        <button onClick={() => dispatch({ type: "inc" })}>+</button>
       </div>
 
       <p>{date.toDateString()}</p>
 
       <div>
-        <button onClick={reset}>Reset</button>
+        <button
+          onClick={() => dispatch({ type: "reset", payload: initialState })}
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
