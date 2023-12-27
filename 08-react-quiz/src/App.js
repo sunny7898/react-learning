b320bb65304e75b41ee1dc8019961e74a8a54b10
Sunny Goswami <sungoswami07gmail.com>
2023-12-27 15:31:05 +0530

    fake-api - initial setup - data fetch and useReducer

--- 08-react-quiz/src/App.js
+++ 08-react-quiz/src/App.js
@@ -1,9 +1,46 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  /* 
+    'loading' : in the beginning our application will be in loading state
+    'error'   : if there is any error while fetching the data
+    'ready'   : we have received the data, and ready to start the quiz
+    'active'  : once the quiz is actually running
+    'finished': once the quiz is completed
+  */
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action received is unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8965/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
