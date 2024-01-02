06801c94b46a4ad6b4f1a819ba30133a4e76c896
Sunny Goswami <sungoswami07gmail.com>
2024-01-03 00:13:49 +0530

    nested routes

---
+++ 09-worldwise/src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <Outlet />
+      <footer className={styles.footer}>
+        &copy; Copyright {new Date().getFullYear()} by WorldwWise Inc.
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
