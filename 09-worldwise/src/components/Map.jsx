524d4c3116c3922650216306af13408dc8bdbcd4
Sunny Goswami <sungoswami07gmail.com>
2024-01-06 20:54:44 +0530

    Setting map position with geo location

--- 09-worldwise/src/components/Map.jsx
+++ 09-worldwise/src/components/Map.jsx
@@ -12,10 +12,18 @@ import styles from "./Map.module.css";
 import { useState } from "react";
 import { useCities } from "../../contexts/CitiesContext";
 import { useEffect } from "react";
+import { useGeoLocation } from "../hooks/useGeoLocation";
+import Button from "./Button";
 
 function Map() {
-  const [mapPosition, setMapPosition] = useState([40, 0]);
   const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeoLocation();
+
   const [searchParams] = useSearchParams();
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
@@ -26,14 +34,27 @@ function Map() {
     },
     [mapLat, mapLng]
   );
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
 
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
+
       <MapContainer
-        className={styles.map}
         center={mapPosition}
         zoom={12}
         scrollWheelZoom={true}
+        className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -60,12 +81,11 @@ function Map() {
 
 function ChangeCenter({ position }) {
   const map = useMap();
-  map.setView(position, 12);
+  map.setView(position);
   return null;
 }
 function DetectClick() {
   const navigate = useNavigate();
-
   useMapEvents({
     click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
   });
--- geo-location/src/App.js
+++ geo-location/src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-function useGeolocation() {
+function useGeolocation(defaultPosition = null) {
   const [isLoading, setIsLoading] = useState(false);
-  const [position, setPosition] = useState({});
+  const [position, setPosition] = useState(defaultPosition);
   const [error, setError] = useState(null);
 
   function getPosition() {
