481f7e9c2c32b40d06c47e485152b5ca83654ddc
Sunny Goswami <sungoswami07gmail.com>
2024-01-08 02:36:33 +0530

    cleanup

--- 09-worldwise/contexts/CitiesContext.jsx
+++ 09-worldwise/contexts/CitiesContext.jsx
@@ -1,6 +1,6 @@
 import { useReducer } from "react";
 import { useContext } from "react";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect } from "react";
 
 const CitiesContext = createContext();
 
--- 09-worldwise/contexts/FakeAuthContext.jsx
+++ 09-worldwise/contexts/FakeAuthContext.jsx
@@ -1,7 +1,6 @@
 import { useReducer } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
-import { useNavigate } from "react-router";
 
 const AuthContext = createContext();
 
