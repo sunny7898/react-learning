98dbaf1b62acec7415a85dc21a58a4dd1e386393
Sunny Goswami <sungoswami07gmail.com>
2024-01-13 19:11:27 +0530

    redux  devtools

--- 11-redux-intro/src/store.js
+++ 11-redux-intro/src/store.js
@@ -1,5 +1,7 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
@@ -8,6 +10,9 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+const store = legacy_createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
